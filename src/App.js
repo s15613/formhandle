414592643f1aea39e6fe38278fa11a053f5d902b
s15613 <dutaitu92@gmail.cogit config --global user.name s15613git statusqgitgit commit -m 'git test'git commit -m 'git test'git config --global user.name s15613git config --global user.email dutaitu92@gmail.co>
2019-09-15 01:49:43 +0900

    form handling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [gender, setGender] = useState('female')
+  const [a, setA] = useState('code')
+  const [check, setCheck] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+
+      <select name='gender'  value={gender} onChange={e => setGender(e.target.value)} >
+            <option value='male' >Male</option>
+            <option value='female' >Female</option>
+      </select>
+
+      <br /> <br /> <br />
+
+        <input type="radio" name="a" value="game" checked={a=== 'game'} onChange={e => setA(e.target.value)} /> Game<br />
+        <input type="radio" name="a" value="code" checked={a=== 'code'} onChange={e => setA(e.target.value)} /> Code<br />
+        <input type="radio" name="a" value="other" checked={a=== 'other'} onChange={e => setA(e.target.value)} /> Other
+
+        <br /> <br /> <br />
+
+        <input 
+            type="checkbox" 
+            name="check"
+            checked={check} 
+            onChange={e => setCheck(e.target.checked)}
+         /> I have a bike<br />
     </div>
+
   );
 }
 
